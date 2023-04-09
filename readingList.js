b6cd4cd905b0d5b2877418c0c791d8033aa595d5
Atique98 <mohammad.atique@xloopdigital.com>
2023-04-09 13:02:57 +0500

    3rd one

--- readingList.js
+++ readingList.js
@@ -3,3 +3,11 @@ let readingList = []
 exports.getBook = ()=>{
     return readingList;
 }
+
+//exports.addBook = (bookDetails,readDate,rating) =>{
+    //readingList.push({...bookDetails,readDate,rating})
+
+exports.numRead = ()=>{
+    return readingList.length
+}
+}
--- test/bookapiUnitTesting.test.js
+++ test/bookapiUnitTesting.test.js
@@ -1,9 +1,43 @@
-const  { getBook } = require('../readingList');
-test('Return a Empty List', ()=>{
+const  { getBook, addBook } = require('../readingList');
+/*test('Return a Empty List', ()=>{
     // Arrange
     const bookList = [];
     //Act
     const list = getBook();
     //Assert
     expect(list).toEqual(bookList)
+}) */
+
+/*3 - Given that I have an empty list, when I add additional books to the list 
+I expect the numberRead to return the total number of books in my list.*/
+
+test('Read Additional Books In List' , ()=>{
+    //Arrange
+/*const bookDetails = {
+    title : "Book Name",
+    author : "Author",
+    page : 500,
+    year: 1999,
+}*/
+
+  //Act
+ const readDate = "21 March 2023";
+ const rating = 3;
+ const ixpected = 2;
+
+
+ const bookDetails = {
+    book1:{ title:"prey" , author:"jenny" , page: 230 , year:2000},
+    book2:{ title:"alculus" , author:"Howord Anton" , page: 600 , year:2012}
+ }
+  const bookinfor = {
+    book1_intro:{readDate: "21 March 2030" , rating: 3},
+    book2_intro:{readDate: "21 March 2030" , rating: 4}
+}
+  addBook(bookDetails.book1,bookinfor.book1_intro);
+  addBook(bookDetails.book2,bookinfor.book2_intro)
+
+  const result = readingList.length;
+    // Assert
+    expect(ixpected).toBe(result)
 })
