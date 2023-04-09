27947e850d4ebc7323cf03651f0a60cd7caac196
Atique98 <mohammad.atique@xloopdigital.com>
2023-04-09 14:28:39 +0500

    3rd pass test

--- readingList.js
+++ readingList.js
@@ -4,10 +4,15 @@ exports.getBook = ()=>{
     return readingList;
 }
 
-//exports.addBook = (bookDetails,readDate,rating) =>{
-    //readingList.push({...bookDetails,readDate,rating})
-
-exports.numRead = ()=>{
-    return readingList.length
+exports.addBook= (bookDetails,readDate,rating) =>{
+   bookDetails.readDate=readDate;
+   bookDetails.rating=rating;
+   readingList.push({bookDetails});
 }
+
+exports.numRead=() => {
+    return readingList.length;
+}
+exports.addBooks=(bookDetails,bookinfor)=>{
+    readingList.push({...bookDetails,bookinfor});
 }
