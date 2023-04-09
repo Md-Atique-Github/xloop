f52df540cb88dafa2ed68369903860d9fdadecdc
Atique98 <mohammad.atique@xloopdigital.com>
2023-04-09 17:58:52 +0500

    final set pass tests

--- test/bookapiUnitTesting.test.js
+++ test/bookapiUnitTesting.test.js
@@ -1,12 +1,18 @@
 const  { getBook, numRead ,addBooks} = require('../readingList');
-/*test('Return a Empty List', ()=>{
+
+// Written BY Muhammad Atique
+
+/*Given that I visit the site, when I first start, I expect my list to be empty.*/
+
+test('Return a Empty List', ()=>{
     // Arrange
     const bookList = [];
     //Act
     const list = getBook();
     //Assert
     expect(list).toEqual(bookList)
-}) */
+}) 
+// Written BY Muhammad Atique
 
 /*3 - Given that I have an empty list, when I add additional books to the list 
 I expect the numberRead to return the total number of books in my list.*/
@@ -32,3 +38,21 @@ test('Read Additional Books In List' , ()=>{
     // Assert
     expect(axpected).toBe(result)
 })
+
+// Written BY Muhammad Atique
+
+/* 5 - Given that I have an empty list, when I add a new book
+ I expect getBooks() to return a list of books that includes the book I added.*/
+
+ test('when I add a new book I expect getbooks() to return list of book', ()=>{
+  //Arrange
+const expectedbook = [{author: "jenny", bookinfor: {rating: 3, readDate: "21 March 2030"}, page: 230, title: "prey", year: 2000}, {author: "Howord Anton", bookinfor: {rating: 4, readDate: "21 March 2030"}, page: 600, title: "alculus", 
+year: 2012}, {author: " Zeeshan ul Hassan Usmani", bookinfor: "Mar 21 , 2023", lenght: 1000, title: "justojoo ka safr", year: 1999}];
+  //Act
+const book =  {title:'justojoo ka safr',author:' Zeeshan ul Hassan Usmani' ,lenght: 1000,year:1999}
+
+addBooks(book, 'Mar 21 , 2023', 3 )
+const recordedBook = getBook();
+  //Assert
+ expect(expectedbook).toEqual(recordedBook)
+ })
